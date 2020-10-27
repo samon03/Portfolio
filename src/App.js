d23efa17b75e7d9f2c03e07299f4470223b18b39
Samon03 <samon3123@gmail.com>
2020-10-28 03:17:28 +0600

    Carousel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import MyNavbar from './components/myNavbar/myNavbar.component';
+import MyCarousel from './components/my-carousel/my-carousel.component';
+import TitleMessage from './components/title-message/title-message.component';
 
 const App = () => {
   return (
     <div className="App">
       <MyNavbar/>
+      <MyCarousel/>
+      <TitleMessage/>
     </div>
   );
 }
